c378df955b0113ba8014f25e59731979c7a39459
Minjin <minjinnie7@gmail.com>
2025-03-20 23:19:26 -0600

    Update metrics.js

--- src/metrics.js
+++ src/metrics.js
@@ -13,8 +13,8 @@ class MetricBuilder {
   }
 
   addNewMetric(metricName, metricValue, type, unit, attributes) {
-    const value =(!metricValue || isNaN(metricValue)) ? 0 : metricValue;
-    this.metrics.push(getSingleMetric(metricName, value, type, unit, attributes))
+    const value = (!metricValue || isNaN(metricValue)) ? 0 : metricValue;
+    this.metrics.push(getSingleMetric(metricName, value, type, unit, attributes));
   }
 
   getAllMetrics() {
@@ -28,12 +28,11 @@ const pizzas = {};
 const latency = {};
 
 function track(endpoint) {
-    return (req, res, next) => {
-      updateMetric(requests, endpoint);
-      next();
-    };
-  }
-  
+  return (req, res, next) => {
+    updateMetric(requests, endpoint);
+    next();
+  };
+}
 
 function trackFail() {
   return (err, req, res, next) => {
@@ -48,7 +47,7 @@ function trackSuccess() {
 }
 
 function trackActive(active) {
-  updateMetric(authentication, 'active', (active ? 1 : -1))
+  updateMetric(authentication, 'active', (active ? 1 : -1));
 }
 
 function trackPizza(metric, value) {
@@ -56,7 +55,7 @@ function trackPizza(metric, value) {
 }
 
 function trackLatency(key, time) {
-  latency[key] = [...latency[key] ?? [], time];
+  latency[key] = [...(latency[key] ?? []), time];
 }
 
 function updateMetric(metric, key, value) {
@@ -76,46 +75,43 @@ function getMemoryUsagePercentage() {
   return memoryUsage.toFixed(2);
 }
 
+if (process.env.NODE_ENV !== 'test') {
+  setInterval(() => {
+    const builder = new MetricBuilder();
 
-setInterval(() => {
-  const builder = new MetricBuilder();
+    builder.addMetrics('requests', requests, 'sum', '1');
+    builder.addMetrics('authentication', authentication, 'sum', '1');
+    builder.addNewMetric('cpu', getCpuUsagePercentage(), 'gauge', '%');
+    builder.addNewMetric('memory', getMemoryUsagePercentage(), 'gauge', '%');
+    builder.addMetrics('pizzas', pizzas, 'sum', '1');
 
-  builder.addMetrics('requests', requests, 'sum', '1');
-  builder.addMetrics('authentication', authentication, 'sum', '1');
-  builder.addNewMetric('cpu', getCpuUsagePercentage(), 'gauge', '%');
-  builder.addNewMetric('memory', getMemoryUsagePercentage(), 'gauge', '%');
-  builder.addMetrics('pizzas', pizzas, 'sum', '1');
+    Object.keys(latency).forEach((key) => {
+      const list = latency[key] ?? [];
+      const avg = list.length > 0 ? list.reduce((a, b) => a + b, 0) / list.length : 0;
+      builder.addNewMetric('latency_milliseconds_total', avg, 'sum', 'ms', { key });
+      latency[key] = [];
+    });
 
-  Object.keys(latency).forEach((key) => {
-    builder.addNewMetric('latency', (latency[key].reduce((partial, a) => partial + a, 0)) / latency[key].length, 'sum', 'ms', { key });
-    latency[key] = [];
-  });
-
-  sendToGrafana(builder.getAllMetrics(), 'all');
-}, 1000);
+    sendToGrafana(builder.getAllMetrics(), 'all');
+  }, 1000);
+}
 
 function getMetricsBody(...allMetrics) {
-  const metrics = {
+  return {
     resourceMetrics: [
       {
         scopeMetrics: [
           {
-            metrics: []
+            metrics: allMetrics
           },
         ],
       },
     ],
   };
-
-  allMetrics.forEach((metric) => {
-    metrics.resourceMetrics[0].scopeMetrics[0].metrics.push(metric);
-  });
-
-  return metrics;
 }
 
 function getSingleMetric(metricName, metricValue, type, unit, attributes) {
-  attributes = { ...attributes, source: config.metrics.source }
+  attributes = { ...attributes, source: config.metrics.source };
 
   const metric = {
     name: metricName,
@@ -125,39 +121,43 @@ function getSingleMetric(metricName, metricValue, type, unit, attributes) {
         {
           asDouble: metricValue,
           timeUnixNano: Date.now() * 1000000,
-          attributes: [],
+          attributes: Object.entries(attributes).map(([key, value]) => ({
+            key,
+            value: { stringValue: value }
+          })),
         },
       ]
     },
   };
 
-  if (type == 'sum') {
+  if (type === 'sum') {
     metric[type].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
     metric[type].isMonotonic = true;
   }
-
-  Object.keys(attributes).forEach((key) => {
-    metric[type].dataPoints[0].attributes.push({
-      key: key,
-      value: { stringValue: attributes[key] },
-    });
-  });
 
   return metric;
 }
 
 function sendToGrafana(metric, metricName) {
   const body = JSON.stringify(metric);
+
   fetch(`${config.metrics.url}`, {
     method: 'POST',
-    body: body,
-    headers: { Authorization: `Bearer ${config.metrics.apiKey}`, 'Content-Type': 'application/json' },
+    body,
+    headers: {
+      Authorization: `Bearer ${config.metrics.apiKey}`,
+      'Content-Type': 'application/json'
+    },
   })
     .then((response) => {
       if (!response.ok) {
-        response.text().then((text) => {
-          console.error(`Failed to push metrics data to Grafana: ${text}\n${body}`);
-        });
+        if (typeof response.text === 'function') {
+          response.text().then((text) => {
+            console.error(`Failed to push metrics data to Grafana: ${text}\n${body}`);
+          });
+        } else {
+          console.error('Failed to push metrics data to Grafana: Unknown error');
+        }
       } else {
         console.log(`Pushed ${metricName}`);
       }
@@ -167,4 +167,11 @@ function sendToGrafana(metric, metricName) {
     });
 }
 
-module.exports = { track, trackSuccess, trackFail, trackActive, trackPizza, trackLatency };
+module.exports = {
+  track,
+  trackSuccess,
+  trackFail,
+  trackActive,
+  trackPizza,
+  trackLatency
+};
